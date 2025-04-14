5b3127322d2c5773cb1227ec97ebd414f130afd5
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-04-14 17:03:29 +0530

    Update route.ts

--- app/api/workflows/execute/route.ts
+++ app/api/workflows/execute/route.ts
@@ -1,82 +1,92 @@
 import prisma from "@/lib/prisma";
 import { ExecuteWorkflow } from "@/lib/workflow/executeWorkflow";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
-import { ExecutionPhaseStatus, WorkflowExecutionPlan, WorkflowExecutionStatus, WorkflowExecutionTrigger } from "@/types/workflow";
+import {
+  ExecutionPhaseStatus,
+  WorkflowExecutionPlan,
+  WorkflowExecutionStatus,
+  WorkflowExecutionTrigger,
+} from "@/types/workflow";
 import { timingSafeEqual } from "crypto";
-import { CronExpressionParser } from 'cron-parser';
+import { CronExpressionParser } from "cron-parser";
 
+export const dynamic = "force-dynamic";
 
-function isValidSecret(secret:string){
-    const API_SECRET=process.env.API_SECRET;
-   if (!API_SECRET) return false;
-    try{
-        return timingSafeEqual(Buffer.from(secret),Buffer.from(API_SECRET));
-    }catch(error){
-        return false;  
-    }
+function isValidSecret(secret: string) {
+  const API_SECRET = process.env.API_SECRET;
+  if (!API_SECRET) return false;
+
+  try {
+    return timingSafeEqual(Buffer.from(secret), Buffer.from(API_SECRET));
+  } catch (error) {
+    return false;
+  }
 }
-export async function GET(request:Request){
-    const authHeader=request.headers.get("authorization");
 
-    if (!authHeader || !authHeader.startsWith("Bearer "))
-        return Response.json({error:"Unauthorized"},{status:401})
-  
-    const secret=authHeader.split(" ")[1];
-    if (!isValidSecret(secret)){
-        return Response.json({error:"Unauthorized"},{status:401});
-    }
-    const {searchParams}=new URL(request.url);
-    const workflowId=searchParams.get("workflowId") as string; 
+export async function POST(request: Request) {
+  const authHeader = request.headers.get("authorization");
 
-    if (!workflowId){
-        return Response.json({error:"bad request"},{status:400});
-    }
-    const workflow=await prisma.workflow.findUnique({
-        where:{id:workflowId},
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const secret = authHeader.split(" ")[1];
+  if (!isValidSecret(secret)) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const workflowId = searchParams.get("workflowId") as string;
+
+  if (!workflowId) {
+    return Response.json({ error: "Bad request" }, { status: 400 });
+  }
+
+  const workflow = await prisma.workflow.findUnique({
+    where: { id: workflowId },
+  });
+
+  if (!workflow || !workflow.executionPlan) {
+    return Response.json({ error: "Bad request" }, { status: 400 });
+  }
+
+  const executionPlan = JSON.parse(
+    workflow.executionPlan
+  ) as WorkflowExecutionPlan;
+
+  let nextRun: Date;
+  try {
+    const cron = CronExpressionParser.parse(workflow.cron!);
+    nextRun = cron.next().toDate();
+
+    const execution = await prisma.workflowExecution.create({
+      data: {
+        workflowId,
+        userId: workflow.userId,
+        definition: workflow.definition,
+        status: WorkflowExecutionStatus.PENDING,
+        startedAt: new Date(),
+        trigger: WorkflowExecutionTrigger.CRON,
+        phases: {
+          create: executionPlan.flatMap((phase) => {
+            return phase.nodes.flatMap((node) => {
+              return {
+                userId: workflow.userId,
+                status: ExecutionPhaseStatus.CREATED,
+                number: phase.phase,
+                node: JSON.stringify(node),
+                name: TaskRegistry[node.data.type].label,
+              };
+            });
+          }),
+        },
+      },
     });
 
-    if (!workflow){
-        return Response.json({error:"bad request"},{status:400});
-    }
-
-    const executionPlan=JSON.parse(workflow.executionPlan!) as WorkflowExecutionPlan;
-
-    if (!executionPlan){
-        return Response.json({error:"bad request"},{status:400});
-    }
-    let nextRun;
-    try{
-        const cron=CronExpressionParser.parse(workflow.cron!);
-        const nextRun=cron.next().toDate();
-        const execution=await prisma.workflowExecution.create({
-            data:{
-                workflowId,
-                userId:workflow.userId,
-                definition:workflow.definition,
-                status:WorkflowExecutionStatus.PENDING,
-                startedAt:new Date(),
-                trigger: WorkflowExecutionTrigger.CRON,
-                 phases: {
-                            create: executionPlan.flatMap((phase)=>{
-                                return phase.nodes.flatMap((node)=>{
-                                    return {
-                                     userId:workflow.userId,
-                                     status:ExecutionPhaseStatus.CREATED,
-                                     number:phase.phase,
-                                     node:JSON.stringify(node),
-                                     name:TaskRegistry[node.data.type].label,
-                                    }
-                                });
-                            })
-                        }
-            }
-        });
-        await ExecuteWorkflow(execution.id,nextRun);
-        return new Response(null,{status:200});  
-    }catch(error){  
-        return Response.json({error:"internal server error"},{status:500});   
-    }
-
-
-    
+    await ExecuteWorkflow(execution.id, nextRun);
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    console.error("Error executing workflow:", error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
