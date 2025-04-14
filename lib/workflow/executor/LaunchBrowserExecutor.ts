5678af35fa2abd2f86350bdc02ab2854e09e9fe2
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-04-14 23:54:28 +0530

    Update LaunchBrowserExecutor.ts

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,35 +1,26 @@
-import puppeteer from "puppeteer-core";
 import { Environment, ExecutionEnvironment } from "@/types/executor";
+import puppeteer from "puppeteer";
 import { LaunchBrowserTask } from "../task/LaunchBrowser";
-import { getOptions } from "../../puppeteerOptions";
 
-
-export async function LaunchBrowserExecutor(
-  environment: ExecutionEnvironment<typeof LaunchBrowserTask>
-): Promise<boolean> {
-  try {
-    const websiteUrl = environment.getInput("Website URL");
-
-    if (!websiteUrl) {
-      throw new Error("Website URL is not being passed properly. It's empty.");
+export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typeof LaunchBrowserTask>):Promise<boolean>{
+    try {
+        const websiteUrl = environment.getInput("Website URL"); 
+        if (!websiteUrl){
+            throw new Error("Website url is not being passed properly its empty")
+        }
+        const browser=await puppeteer.launch({
+            headless:true, 
+        });           
+        environment.log.info("Browser started successfully");
+        environment.setBrowser(browser);
+        const page=await browser.newPage();
+        await page.goto(websiteUrl);
+        environment.setPage(page);
+        environment.log.info(`Opened page at:${websiteUrl}`)
+        return true;
     }
-
-
-    const options = await getOptions(); 
-    const browser = await puppeteer.launch(options);
-
-    environment.log.info("Browser started successfully");
-    environment.setBrowser(browser);
-
-    const page = await browser.newPage();
-    await page.goto(websiteUrl);
-
-    environment.setPage(page);
-    environment.log.info(`Opened page at: ${websiteUrl}`);
-
-    return true;
-  } catch (error: any) {
-    environment.log.error(error.message);
-    return false;
-  }
-}
+   catch(error:any){
+       environment.log.error(error.message);
+       return false;
+   }
+} 
