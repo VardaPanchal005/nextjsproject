b9bcd991f42d8ef46eacb418c96607c5c6af1983
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-04-15 00:01:18 +0530

    Update LaunchBrowserExecutor.ts

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,26 +1,35 @@
+import puppeteer from "puppeteer-core";
 import { Environment, ExecutionEnvironment } from "@/types/executor";
-import puppeteer from "puppeteer";
 import { LaunchBrowserTask } from "../task/LaunchBrowser";
+import { getOptions } from "../../puppeteerOptions";
 
-export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typeof LaunchBrowserTask>):Promise<boolean>{
-    try {
-        const websiteUrl = environment.getInput("Website URL"); 
-        if (!websiteUrl){
-            throw new Error("Website url is not being passed properly its empty")
-        }
-        const browser=await puppeteer.launch({
-            headless:true, 
-        });           
-        environment.log.info("Browser started successfully");
-        environment.setBrowser(browser);
-        const page=await browser.newPage();
-        await page.goto(websiteUrl);
-        environment.setPage(page);
-        environment.log.info(`Opened page at:${websiteUrl}`)
-        return true;
+
+export async function LaunchBrowserExecutor(
+  environment: ExecutionEnvironment<typeof LaunchBrowserTask>
+): Promise<boolean> {
+  try {
+    const websiteUrl = environment.getInput("Website URL");
+
+    if (!websiteUrl) {
+      throw new Error("Website URL is not being passed properly. It's empty.");
     }
-   catch(error:any){
-       environment.log.error(error.message);
-       return false;
-   }
-} 
+
+
+    const options = await getOptions(); 
+    const browser = await puppeteer.launch(options);
+
+    environment.log.info("Browser started successfully");
+    environment.setBrowser(browser);
+
+    const page = await browser.newPage();
+    await page.goto(websiteUrl);
+
+    environment.setPage(page);
+    environment.log.info(`Opened page at: ${websiteUrl}`);
+
+    return true;
+  } catch (error: any) {
+    environment.log.error(error.message);
+    return false;
+  }
+}
