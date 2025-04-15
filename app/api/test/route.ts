435f1f00bf6da62742c66734336dfe764deb2b14
VardaPanchal005 <vardapanchal@gmail.com>
2025-04-15 23:06:04 +0530

    update test route to send error

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,15 +3,10 @@ import { NextRequest } from "next/server";
 import puppeteer from "puppeteer-core";
 
 export async function GET(req:NextRequest){
-    try {
-        const options = await getOptions();
-        const browser = await puppeteer.launch(options);
-        
-        const page = await browser.newPage();
-        await page.goto("https://quotes.toscrape.com/login");
-        return Response.json({success: `${page.url()} visited`});
-    } catch (error) {
-        return Response.json({error})
-    }
-
+    const options = await getOptions();
+    const browser = await puppeteer.launch(options);
+    
+    const page = await browser.newPage();
+    await page.goto("https://quotes.toscrape.com/login");
+    return Response.json({success: `${page.url()} visited`});
 }
