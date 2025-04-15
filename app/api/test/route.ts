9c49f7d2d039d5bb193db2d599017e852dc1d8b7
VardaPanchal005 <vardapanchal@gmail.com>
2025-04-15 23:56:41 +0530

    .

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,12 +6,12 @@ export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
 
-//     const remoteExecutablePath =
-//  "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar"
+    const remoteExecutablePath =
+ "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar"
     const options:PuppeteerLaunchOptions = {
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
+        executablePath: await chromium.executablePath(remoteExecutablePath),
         headless: chromium.headless,
     }
   const browser = await puppeteer.launch(options);
