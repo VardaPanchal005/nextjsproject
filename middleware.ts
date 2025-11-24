925099e895e73a9201713f39a9be8990e3ab1cac
VardaPanchal005 <114830938+VardaPanchal005@users.noreply.github.com>
2025-11-24 12:51:35 +0530

    Update public route matcher for keepmealive

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/api/workflows/(.*)*',
-  '/api/keepmealive'
+  '/api/keepmealive(.*)'
 ]);
 export default clerkMiddleware(async (auth, request) => {
   if (isPublicRoute(request)) {
@@ -22,7 +22,7 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+   '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 };
